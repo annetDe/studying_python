eb4be4b0b6be5580d88e792d934be0daf3a96108
Hanna Demydenko <hanna.demydenko@gmail.com>
2020-12-15 17:57:47 +0200

    stat save at file

--- coursework/main.py
+++ coursework/main.py
@@ -1,3 +1,5 @@
+import json
+
 from player import Player
 from magic import magic_rule, magic_game
 from blackjack import blackjack_rule, blackjack_game
@@ -5,38 +7,90 @@ from blackjack import blackjack_rule, blackjack_game
 
 def main():
     name = input('Enter your name: ')
-    player = Player(name)
+
+    try:
+        with open('stat.json') as f:
+            players_data = json.load(f)
+    except FileNotFoundError:
+        players_data = []
+
+    player = isexisted_player(players_data, name)
 
     casino_continue = 'y'
     while casino_continue == 'y':
         choice = 0
         while choice not in (1, 2, 3, 4, 5):
             try:
-                choice = int(input(
-                    '\nMain menu:'
-                    '\n1 Magic'
-                    '\n2 Blackjack (21)'
-                    '\n3 View statistics'
-                    '\n4 Reset game progress'
-                    '\n5 Leave the casino\n'
-                ))
+                choice = main_menu()
             except ValueError:
                 continue
 
         if choice == 1:
             magic_rule()
             magic_game(player)
+
         elif choice == 2:
             blackjack_rule()
             blackjack_game(player)
+
         elif choice == 3:
             player.print_stat()
+
         elif choice == 4:
+            for d in players_data:
+                if d['name'] == name:
+                    players_data.remove(d)
+                    break
             del player
             name = input('Enter your name: ')
-            player = Player(name)
-        else:
+            player = isexisted_player(players_data, name)
+
+        elif choice == 5:
+            existed_player = 0
+            new_players_data = players_data
+            for d in new_players_data:
+                if d['name'] == name:
+                    d.update(player.__dict__)
+                    existed_player = 1
+                    break
+            if existed_player == 0:
+                new_players_data.append(player.__dict__)
+
+            with open('stat.json', 'w') as f:
+                data = json.dumps(new_players_data, indent=4)
+                f.write(data)
+
             casino_continue = 'n'
+
+
+def main_menu():
+    choice = int(input(
+        '\nMain menu:'
+        '\n1 Magic'
+        '\n2 Blackjack (21)'
+        '\n3 View statistics'
+        '\n4 Reset game progress'
+        '\n5 Leave the casino\n'
+    ))
+    return choice
+
+
+def isexisted_player(players_data, name):
+    tmp = next((i for i in players_data if i['name'] == name), {})
+    if tmp:
+        player = Player(
+                        tmp['name'],
+                        tmp['points'],
+                        tmp['m_games_won'],
+                        tmp['m_games_lose'],
+                        tmp['record_counter'],
+                        tmp['b_games_won'],
+                        tmp['b_games_lose'],
+                        tmp['b_games_return']
+        )
+    else:
+        player = Player(name)
+    return player
 
 
 main()
--- coursework/player.py
+++ coursework/player.py
@@ -8,8 +8,16 @@ class Player:
     b_games_lose = 0
     b_games_return = 0
 
-    def __init__(self, name):
+    def __init__(self, name, points=10, m_games_won=0, m_games_lose=0,
+    record_counter=999, b_games_won=0, b_games_lose=0, b_games_return=0):
         self.name = name
+        self.points = points
+        self.m_games_won = m_games_won
+        self.m_games_lose = m_games_lose
+        self.record_counter = record_counter
+        self.b_games_won = b_games_won
+        self.b_games_lose = b_games_lose
+        self.b_games_return = b_games_return
 
     @property
     def m_games_played(self):
